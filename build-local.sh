6cd99b4682c8c8206a615abe30a618b327195a9c
Drsdre <github.com@drsdre.org>
2020-04-08 16:29:47 +0200

    Use install-php-extensions to install xdebug container. Removed git from container. Removed old PHP 7.1.

--- build-local.sh
+++ build-local.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-docker build . -f 7.1/alpine/cli/Dockerfile -t drsdre/php-laravel-dev:7.1-cli-alpine
-docker build . -f 7.1/alpine/fpm/Dockerfile -t drsdre/php-laravel-dev:7.1-fpm-alpine
 docker build . -f 7.2/alpine/cli/Dockerfile -t drsdre/php-laravel-dev:7.2-cli-alpine
 docker build . -f 7.2/alpine/fpm/Dockerfile -t drsdre/php-laravel-dev:7.2-fpm-alpine
 docker build . -f 7.3/alpine/cli/Dockerfile -t drsdre/php-laravel-dev:7.3-cli-alpine
