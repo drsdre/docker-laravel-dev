cb4489179e177c3329cf5d1912acae540a1451a4
Drsdre <github.com@drsdre.org>
2020-04-09 11:20:38 +0200

    Make build-local.sh and push-docker.sh stop at first failure.

--- build-local.sh
+++ build-local.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-docker build . -f 7.2/alpine/cli/Dockerfile -t drsdre/php-laravel-dev:7.2-cli-alpine
-docker build . -f 7.2/alpine/fpm/Dockerfile -t drsdre/php-laravel-dev:7.2-fpm-alpine
-docker build . -f 7.3/alpine/cli/Dockerfile -t drsdre/php-laravel-dev:7.3-cli-alpine
-docker build . -f 7.3/alpine/fpm/Dockerfile -t drsdre/php-laravel-dev:7.3-fpm-alpine
-docker build . -f 7.3/alpine/fpm-nginx/Dockerfile -t drsdre/php-laravel-dev:7.3-fpm-nginx-alpine
-docker build . -f 7.4/alpine/cli/Dockerfile -t drsdre/php-laravel-dev:7.4-cli-alpine
-docker build . -f 7.4/alpine/fpm/Dockerfile -t drsdre/php-laravel-dev:7.4-fpm-alpine
-docker build . -f 7.4/alpine/fpm-nginx/Dockerfile -t drsdre/php-laravel-dev:7.4-fpm-nginx-alpine
+docker build . -f 7.2/alpine/cli/Dockerfile -t drsdre/php-laravel-dev:7.2-cli-alpine \
+&& docker build . -f 7.2/alpine/fpm/Dockerfile -t drsdre/php-laravel-dev:7.2-fpm-alpine \
+&& docker build . -f 7.3/alpine/cli/Dockerfile -t drsdre/php-laravel-dev:7.3-cli-alpine \
+&& docker build . -f 7.3/alpine/fpm/Dockerfile -t drsdre/php-laravel-dev:7.3-fpm-alpine \
+&& docker build . -f 7.3/alpine/fpm-nginx/Dockerfile -t drsdre/php-laravel-dev:7.3-fpm-nginx-alpine \
+&& docker build . -f 7.4/alpine/cli/Dockerfile -t drsdre/php-laravel-dev:7.4-cli-alpine \
+&& docker build . -f 7.4/alpine/fpm/Dockerfile -t drsdre/php-laravel-dev:7.4-fpm-alpine \
+&& docker build . -f 7.4/alpine/fpm-nginx/Dockerfile -t drsdre/php-laravel-dev:7.4-fpm-nginx-alpine
--- push-docker.sh
+++ push-docker.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-docker push drsdre/php-laravel-dev:7.2-cli-alpine
-docker push drsdre/php-laravel-dev:7.2-fpm-alpine
-docker push drsdre/php-laravel-dev:7.3-cli-alpine
-docker push drsdre/php-laravel-dev:7.3-fpm-alpine
-docker push drsdre/php-laravel-dev:7.3-fpm-nginx-alpine
-docker push drsdre/php-laravel-dev:7.4-cli-alpine
-docker push drsdre/php-laravel-dev:7.4-fpm-alpine
-docker push drsdre/php-laravel-dev:7.4-fpm-nginx-alpine
+docker push drsdre/php-laravel-dev:7.2-cli-alpine \
+&& docker push drsdre/php-laravel-dev:7.2-fpm-alpine \
+&& docker push drsdre/php-laravel-dev:7.3-cli-alpine \
+&& docker push drsdre/php-laravel-dev:7.3-fpm-alpine \
+&& docker push drsdre/php-laravel-dev:7.3-fpm-nginx-alpine \
+&& docker push drsdre/php-laravel-dev:7.4-cli-alpine \
+&& docker push drsdre/php-laravel-dev:7.4-fpm-alpine \
+&& docker push drsdre/php-laravel-dev:7.4-fpm-nginx-alpine
